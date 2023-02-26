732fc19f45cee47130fb378c0b17c2f08cd15bad
Artem Holodyr <artemmholodyr@gmail.com>
2023-02-26 07:29:10 +0200

    initial commit

---
+++ accounts/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import CustomUser
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    list_display = ('username', 'email', 'is_staff')
+    ordering = ('is_staff', 'username', 'email')
+    search_fields = ('username', 'email')
